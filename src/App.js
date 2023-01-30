4e60e64638ef9f60c7c21b4b2d4f234e870c50bf
Pavithran2 <pavithrank02@gmail.com>
2023-01-30 11:53:09 +0530

    Home page created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route index element={<h1>hello world2</h1>}></Route>
+          <Route index element={<Home />}></Route>
           <Route path="movie/:id" element={<h1>Movie Detail Page</h1>}></Route>
           <Route path="movie/:type" element={<h1>Movie List Page</h1>}></Route>
           <Route path="/" element={<h1>Error Page</h1>}></Route>
         </Routes>
       </BrowserRouter>
-      <h1>wait</h1>
     </div>
   );
 }
---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import  React from "react"
+import "./Home.css"
+const Home = () => {
+  return (
+    <>
+      Home Page
+    
+    </>
+  )
+}
+
+export default Home
